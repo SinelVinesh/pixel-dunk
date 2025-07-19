1425a98ea07ecf7b3c4c14bfba1ff8691c473074
SinelVinesh <vinesh.sinel@gmail.com>
2025-07-19 14:45:13 +0300

    🔨 Fix merge bugs

--- ui/VictoryUI/victory_ui.gd
+++ ui/VictoryUI/victory_ui.gd
@@ -2,9 +2,9 @@ extends Control
 
 const victory_text_template = "[wave amp=40.0 freq=10.0 connected=1]%s[/wave]"
 var quitable: bool = false
-var frog_texture = "res://assets/assets/ui/png/Background/background_frog.png"
-var rabbit_texture = "res://assets/assets/ui/png/Background/background_rabbit.png"
-var common_texture = "res://assets/assets/ui/png/Background/background_rabbit_vs_frog.png"
+var frog_texture = "res://assets/ui/png/Background/background_frog.png"
+var rabbit_texture = "res://assets/ui/png/Background/background_rabbit.png"
+var common_texture = "res://assets/ui/png/Background/background_rabbit_vs_frog.png"
 
 signal close_victory_screen
 
@@ -16,16 +16,16 @@ func initialize_victory_screen():
 		Constants.Team.FROG :
 			$VictoryText.text = victory_text_template%"[color=#ff0044]AMPHIBALL WINS ![/color]"
 			$Background.texture = load(frog_texture)
-			$Winner1.play("carry_0")
-			$Winner2.play("carry_0")
+			$Winner1.play("frog_0")
+			$Winner2.play("frog_1")
 		Constants.Team.RABBIT :
 			$VictoryText.text = victory_text_template%"[color=#0095e9]RABBEAST WINS ![/color]"
 			$Background.texture = load(rabbit_texture)
-			$Winner1.play("carry_1")
-			$Winner2.play("carry_1")
+			$Winner1.play("rabbit_0")
+			$Winner2.play("rabbit_1")
 		Constants.Team.NONE :
-			$Winner1.play("carry_1")
-			$Winner2.play("carry_0")
+			$Winner1.play("frog_0")
+			$Winner2.play("rabbit_0")
 			$Background.texture = load(common_texture)
 			$VictoryText.text = victory_text_template%"EVERYBODY WINS !"
 
